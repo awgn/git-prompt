143427bcaf2a481757450682ae4ea3acd37dc83b
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-02-04 14:10:48 +0100

    First commit

---
+++ gitprompt.hs
@@ -0,0 +1,31 @@
+-- git-prompt:  Nicola Bonelli <bonelli@antifork.org>
+-- 
+-- Add the following line to ~/.bashrc
+--
+-- export PS1='\u@\h \[\033[1;32m\]\w\[\033[0m\]$(/usr/local/bin/gitprompt)$ '
+
+import System.Process
+import Data.List
+
+main :: IO ()
+main = do
+       xs <- readProcessWithExitCode "git" ["status"] []
+       let ls = lines $ snd' xs
+       putStr $ prompt (branch ls) (icon ls) 
+       where snd' ( _, xs, _ ) = xs
+
+branch :: [String] -> String
+branch [] = ""
+branch (x:xs) |  "# On branch" `isPrefixOf` x = (words x) !! 3
+              | otherwise = branch xs
+        
+icon :: [String] -> String
+icon [] = ""
+icon (x:xs) | "# Changes to be committed" `isPrefixOf` x = "!"
+            | "# Changes not staged"      `isPrefixOf` x = "*"
+            | "# Untracked files:"        `isPrefixOf` x = "+"
+            | otherwise = icon xs
+
+prompt :: String -> String -> String
+prompt b i | null b = ""
+           | otherwise = "[" ++ b ++ i ++ "]"
